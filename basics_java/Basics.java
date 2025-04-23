074335303e9a7dbaf3a7cc88fbb09aa3bbc2d502
Subramanya <anantha.subramanya@capgemini.com>
2025-04-23 20:33:51 +0530

    basics_java

---
+++ basics_java/Basics.java
@@ -0,0 +1,8 @@
+
+public class Basics {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+        System.out.println("Hello Fucking World");
+        System.out.println("There is no one saving you"); // Print a message to the console
+    }
+}
